1b03951afe11b9c99ea17b98f9794d087c299736
Aaron Aikman <aaikman@blindsquirrelgames.com>
2017-11-28 13:09:49 -0800

    General commit

--- scripts/AAikman_AimConstraintAll.py
+++ scripts/AAikman_AimConstraintAll.py
@@ -22,20 +22,20 @@ python("import AAikman_AimConstraintAll as aaaca; reload(aaaca); aaaca.main(0, T
 import maya.cmds as cmds
 
 def main(useObjUp=0, maintOff=True):
-	sel = cmds.ls(sl=True)
-	oSel = sel
-	parObj = sel[0]
-	sel.pop(0)
+    sel = cmds.ls(sl=True)
+    oSel = sel
+    parObj = sel[0]
+    sel.pop(0)
 
     if useObjUp:
-    	upObj = sel[-1]
-    	sel.pop()
+        upObj = sel[-1]
+        sel.pop()
 
-	for i in sel:
-	    cmds.select(parObj, r=True)
-	    if useObjUp:
-		    cmds.aimConstraint(parObj, i, mo=maintOff, worldUpType=object, worldUpObject=upObj)
-		else:
-		    cmds.aimConstraint(parObj, i, mo=maintOff)
+    for i in sel:
+        cmds.select(parObj, r=True)
+        if useObjUp:
+            cmds.aimConstraint(parObj, i, mo=maintOff, worldUpType="object", worldUpObject=upObj)
+        else:
+            cmds.aimConstraint(parObj, i, mo=maintOff)
 
-	cmds.select(oSel, r=True)
+    cmds.select(oSel, r=True)
