8e48f0698d229d022e4cd6e517640f34e2de42c0
Aaron Aikman <aaikman@blindsquirrelgames.com>
2018-02-26 12:45:53 -0800

    makeArt

---
+++ scripts/AAikman_MakeArt.py
@@ -0,0 +1,56 @@
+'''
+Author:
+Aaron Aikman
+
+Date of Creation:
+2/20/2018
+
+Installation:
+Put in scripts folder
+Enter 'rehash' in the mel command line
+Put the Shelf Button script into a python button
+
+Shelf Button:
+import AAikman_MakeArt as aaMakeArt
+reload(aaMakeArt)
+aaMakeArt.main()
+
+Marking Menu Script:
+python("import AAikman_MakeArt as aaMakeArt; reload(aaMakeArt); aaMakeArt.main();")
+
+'''
+import pymel.core as pm
+dirsCube = [[1,1,1],
+        [1,1,-1],
+        [1,-1,-1],
+        [-1,-1,-1],
+        [-1,-1,1],
+        [-1,1,1],
+        [1,-1,1],
+        [-1,1,-1]]
+
+dirs = [[1,1,1],
+        [1,1,-1],
+        [1,-1,-1],
+        [-1,-1,-1],
+        [-1,-1,1],
+        [-1,1,1],
+        [1,-1,1],
+        [1.5,0,0],
+        [-1.5,0,0],
+        [0,1.5,0],
+        [0,-1.5,0],
+        [0,0,1.5],
+        [0,0,-1.5],
+        [-1,1,-1]]
+
+def main(arms = 1, iters = 10, mag = 5):
+    for i in pm.selected():
+        for b in range(arms):
+            for a in dirs:
+                pm.select(i)
+                for j in range(iters + 1):
+                    k = pm.duplicate()
+                    trans = [x * mag for x in a]
+                    pm.move( trans, r=True)
+                    pm.rotate( '12deg', 0, 0, r=True)
